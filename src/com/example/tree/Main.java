d3479701cf392c0419246ca0dbc3548bb22212ed
GabrielNat1 <dcbiel846@gmail.com>
2025-10-01 17:02:57 -0300

    refactor: rename Leaf -> Node and Tree -> BinaryTree, add inorder traversal

--- src/com/example/tree/Main.java
+++ src/com/example/tree/Main.java
@@ -1,55 +1,9 @@
 package com.example.tree;
 
-class Tree {
-    private Leaf leaf;
-    private Tree left;
-    private Tree right;
-
-    public Tree(){
-        this.leaf = null;
-        this.left = null;
-        this.right = null;
-    }
-
-    public Tree(Leaf leaf) {
-        this.leaf = leaf;
-        this.left = null;
-        this.right = null;
-    }
-
-    public boolean isEmpty(){
-        return this.leaf == null;
-    }
-
-    public void insert(Leaf node){
-        if (isEmpty()){
-            this.leaf = node;
-        } else {
-            Tree newTree= new Tree(node);
-            if (node.getValue() < this.leaf.getValue()){
-                if (this.left == null){
-                    this.left = newTree;
-                    System.out.println("Leaf: " + node.getValue()+ " the left " + this.leaf.getValue());
-                } else {
-                    this.left.insert(node);
-                }
-            } else if(node.getValue() > this.leaf.getValue()){
-                if (this.right == null){
-                    this.right = newTree;
-                    System.out.println("Leaf: " + node.getValue()+ " the right " + this.leaf.getValue());
-                } else {
-                    this.right.insert(node);
-                }
-
-            }
-        }
-    }
-}
-
-class Leaf {
+class Node {
     private int value;
 
-    public Leaf(int value) {
+    public Node(int value) {
         this.value = value;
     }
 
@@ -58,12 +12,72 @@ class Leaf {
     }
 }
 
+class BinaryTree {
+    private Node leaf;
+    private BinaryTree left;
+    private BinaryTree right;
+
+    public BinaryTree() {
+        this.leaf = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public BinaryTree(Node leaf) {
+        this.leaf = leaf;
+        this.left = null;
+        this.right = null;
+    }
+
+    public boolean isEmpty() {
+        return this.leaf == null;
+    }
+
+    public void insert(Node node) {
+        if (isEmpty()) {
+            this.leaf = node;
+        } else {
+            BinaryTree newTree = new BinaryTree(node);
+            if (node.getValue() < this.leaf.getValue()) {
+                if (this.left == null) {
+                    this.left = newTree;
+                    System.out.println("Node " + node.getValue() + " inserted LEFT of " + this.leaf.getValue());
+                } else {
+                    this.left.insert(node);
+                }
+            } else if (node.getValue() > this.leaf.getValue()) {
+                if (this.right == null) {
+                    this.right = newTree;
+                    System.out.println("Node " + node.getValue() + " inserted RIGHT of " + this.leaf.getValue());
+                } else {
+                    this.right.insert(node);
+                }
+            }
+        }
+    }
+
+    public void inorder() {
+        inorderRecursive(this);
+    }
+
+    private void inorderRecursive(BinaryTree node) {
+        if (node != null && !node.isEmpty()) {
+            inorderRecursive(node.left);
+            System.out.print(node.leaf.getValue() + " ");
+            inorderRecursive(node.right);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Tree tree = new Tree(new Leaf(30));
-        tree.insert(new Leaf(15));
-        tree.insert(new Leaf(45));
-        tree.insert(new Leaf(7));
-        tree.insert(new Leaf(19));
+        BinaryTree tree = new BinaryTree(new Node(30));
+        tree.insert(new Node(15));
+        tree.insert(new Node(45));
+        tree.insert(new Node(7));
+        tree.insert(new Node(19));
+
+        System.out.println("Inorder Traversal:");
+        tree.inorder();
     }
 }
